885b75fdf2b1f57257dff2af91d7e93f82c7d53c
YongJae_Kim <bluesink@bluesink.io>
2021-10-18 22:44:15 +0900

    change fifo to double linked list

--- genslave/root/sub_source/static/co_fifo.c
+++ genslave/root/sub_source/static/co_fifo.c
@@ -1,56 +1,107 @@
 #include "co_fifo.h"
 #include "canopen_config.h"
 
-typedef struct{
-  uint8_t head;
-  uint8_t tail;
-  CAN_FRAME fifo[CO_FIFO_NUM];
-} queue_t;
+/***********************************************************************/
+// Canopen frame linked list functions
+/***********************************************************************/
+typedef struct CO_LIST{
+  CAN_FRAME frame;
+  struct CO_LIST *next, *prev;
+}CO_LIST;
 
-bool queue_read(queue_t *queue, CAN_FRAME* elem){
-
-  if(queue->tail == queue->head) return false;
-
-  memcpy(elem, &queue->fifo[queue->tail], sizeof(CAN_FRAME));
-  queue->tail = (queue->tail + 1) % CO_FIFO_NUM;
-  return true;
-
+static inline void CANOpen_list_init(CO_LIST *list){
+  list->next = list;
+  list->prev = list;
 }
 
-bool queue_write(queue_t *queue, CAN_FRAME* elem){
-
-  uint8_t next = (queue->head + 1) % CO_FIFO_NUM;
-  if(next == queue->tail) return false;
-
-  memcpy(&queue->fifo[queue->head], elem, sizeof(CAN_FRAME));
-  queue->head = next;
-
-  return true;
+static inline void CANOpen_list_add(CO_LIST *new,
+  CO_LIST *prev,
+  CO_LIST *next)
+{
+  next->prev = new;
+  new->next = next;
+  new->prev = prev;
+  prev->next = new;
 }
 
+static inline void CANOpen_list_add_next(CO_LIST *new, CO_LIST *head){
+  CANOpen_list_add(new, head, head->next);
+}
 
-static queue_t tx_fifo;
-static queue_t rx_fifo;
+static inline void CANOpen_list_add_prev(CO_LIST *new, CO_LIST *head){
+  CANOpen_list_add(new, head->prev, head);
+}
+
+static inline void CANOpen_list_del(CO_LIST *entry){
+  entry->next->prev = entry->prev;
+  entry->prev->next = entry->next;
+}
+
+/***********************************************************************/
+// Canopen frame FIFO functions
+/***********************************************************************/
+static CO_LIST rxFrame_list[CO_FIFO_NUM];
+static CO_LIST rxFrame_empty_head;
+static CO_LIST rxFrame_filled_head;
+
+static CO_LIST txFrame_list[CO_FIFO_NUM];
+static CO_LIST txFrame_empty_head;
+static CO_LIST txFrame_filled_head;
 
 void Canopen_FIFO_Init(){
-  tx_fifo.head = 0;
-  tx_fifo.tail = 0;
-  rx_fifo.head = 0;
-  rx_fifo.tail = 0;
+  CANOpen_list_init(&rxFrame_empty_head);
+  CANOpen_list_init(&rxFrame_filled_head);
+
+  CANOpen_list_init(&txFrame_empty_head);
+  CANOpen_list_init(&txFrame_filled_head);
+
+  int i;
+  for(i = 0; i < CO_FIFO_NUM; i++){
+    CANOpen_list_add_prev(&rxFrame_list[i], &rxFrame_empty_head);
+    CANOpen_list_add_prev(&txFrame_list[i], &txFrame_empty_head);
+  }
 }
 
 bool Canopen_PutTxFIFO(CAN_FRAME* elem){
-  return queue_write(&tx_fifo, elem);
+  CO_LIST* element = txFrame_empty_head.next;
+  
+  if(element == &txFrame_empty_head) return false;
+  
+  memcpy(&element->frame, elem, sizeof(CAN_FRAME));
+  CANOpen_list_del(element);
+  CANOpen_list_add_prev(element, &txFrame_filled_head);
+  return true;
 }
 
 bool Canopen_GetTxFIFO(CAN_FRAME* elem){
-  return queue_read(&tx_fifo, elem);
+  CO_LIST* element = txFrame_filled_head.next;
+
+  if(element == &txFrame_filled_head) return false;
+
+  memcpy(elem, &element->frame, sizeof(CAN_FRAME));
+  CANOpen_list_del(element);
+  CANOpen_list_add_prev(element, &txFrame_empty_head);
+  return true;
 }
 
 bool Canopen_PutRxFIFO(CAN_FRAME* elem){
-  return queue_write(&rx_fifo, elem);
+  CO_LIST* element = rxFrame_empty_head.next;
+  
+  if(element == &rxFrame_empty_head) return false;
+  
+  memcpy(&element->frame, elem, sizeof(CAN_FRAME));
+  CANOpen_list_del(element);
+  CANOpen_list_add_prev(element, &rxFrame_filled_head);
+  return true;
 }
 
 bool Canopen_GetRxFIFO(CAN_FRAME* elem){
-  return queue_read(&rx_fifo, elem);
+  CO_LIST* element = rxFrame_filled_head.next;
+
+  if(element == &rxFrame_filled_head) return false;
+
+  memcpy(elem, &element->frame, sizeof(CAN_FRAME));
+  CANOpen_list_del(element);
+  CANOpen_list_add_prev(element, &rxFrame_empty_head);
+  return true;
 }
