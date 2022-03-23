af93d6070359fb79f81edfef21aa3ffbc391959c
YongJae_Kim <bluesink@bluesink.io>
2022-03-23 14:07:00 +0900

    add function comment

--- genslave/root/sub_source/appl/user_appl.c
+++ genslave/root/sub_source/appl/user_appl.c
@@ -37,7 +37,10 @@ uint32_t Canopen_Application_AfterGetData(uint16_t Index, uint8_t subIndex){
 {{ user['4'] }}
 /* USER CODE END 4 */
 
-// Call when master wants to reset application
+// Call when canopen event occur @ref CANOPEN_EVENT in canopen_global.h
+// - APPLICATION_RESET_EVENT : NMT apllication reset
+// - COMMUNICATION_RESET_EVENT : NMT communication reset
+// - LIFE_GUARDING_EVENT : Life guarding event
 void Canopen_Application_Event(CANOPEN_EVENT event){
   /* USER CODE BEGIN 5 */
 {{ user['5'] }}
